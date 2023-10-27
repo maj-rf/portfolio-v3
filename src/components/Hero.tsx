9df6cf081f2086134d25efd4e270b2c922f0d41b
bananabread08 <rmkfermin@gmail.com>
2023-10-27 09:48:53 +0800

    update Hero styles, add hero image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,22 +1,46 @@
+import Image from 'next/image';
+
 export const Hero = () => {
   return (
     <section id="About">
-      <div className="max-w-5xl m-auto p-10 flex flex-col gap-4">
-        <h1 className="text-muted-foreground text-xl">
-          Ralph Majed R. Fermin (he/him)
-        </h1>
-        <h2 className="text-lg">
-          I build <s className="decoration-1">and occasionally break stuff</s>.
-        </h2>
-        <p>
-          I’m a Software Developer based in Philippines, building full-stack web
-          applications with TypeScript, React, Node.JS and MySQL / NoSQL.
-        </p>
-        <p>
-          I like playing games especially JRPGs which was instrumental to my
-          journey in learning programming. I want to build tools to enhance the
-          experience and enjoyment in these games.
-        </p>
+      <div className="max-w-4xl m-auto p-10 flex flex-col gap-4">
+        <div className="flex flex-col items-center md:flex-row gap-4 text-center md:text-left">
+          <Image
+            src="/assets/hero_img.jpg"
+            alt="Maj's Profile Picture"
+            width="100"
+            height="100"
+            className="rounded-xl"
+          />
+          <div>
+            <p className="text-lg font-semibold">Ralph Majed R. Fermin</p>
+            <p>Full Stack Developer, Pokemon Trainer</p>
+          </div>
+          <div className="ml-0 md:ml-auto bg-muted px-2 p-1 rounded-lg">
+            rmkfermin@gmail.com
+          </div>
+        </div>
+        <hr></hr>
+        <div className="text-muted-foreground">
+          <h1 className="text-xl text-primary">
+            I build <s className="decoration-1">and occasionally break</s>{' '}
+            stuff.
+          </h1>
+          <p>
+            I’m a Software Developer based in Philippines, building full-stack
+            web applications with{' '}
+            <span className="text-highlight font-semibold">
+              TypeScript, React, Node.JS
+            </span>
+            , and{' '}
+            <span className="text-highlight font-semibold">MySQL / NoSQL</span>.
+          </p>
+          <p>
+            I like playing games especially JRPGs which is instrumental to my
+            journey in learning how to code / program. I want to build tools to
+            enhance the experience and enjoyment in these games.
+          </p>
+        </div>
       </div>
     </section>
   );
