8ddd8011433749bfed773bac48cae5590574c38b
bananabread08 <rmkfermin@gmail.com>
2023-10-29 12:31:34 +0800

    change toggle color

--- src/components/Sidebar/SidebarToggle.tsx
+++ src/components/Sidebar/SidebarToggle.tsx
@@ -5,11 +5,10 @@ export const SidebarToggle = ({ toggleMenu }: { toggleMenu: () => void }) => {
     <Button
       onClick={toggleMenu}
       className="rounded-full fixed top-[25px] left-[25px] z-10 w-[30px] h-[30px] outline-none p-0"
-      variant="secondary"
     >
       <svg width="15" height="15" viewBox="0 0 23 23">
         <motion.path
-          className="stroke-secondary-foreground"
+          className="stroke-secondary"
           strokeWidth="3"
           strokeLinecap="round"
           variants={{
@@ -19,7 +18,7 @@ export const SidebarToggle = ({ toggleMenu }: { toggleMenu: () => void }) => {
         />
         <motion.path
           strokeWidth="3"
-          className="stroke-secondary-foreground"
+          className="stroke-secondary"
           strokeLinecap="round"
           d="M 2 9.423 L 20 9.423"
           variants={{
@@ -29,7 +28,7 @@ export const SidebarToggle = ({ toggleMenu }: { toggleMenu: () => void }) => {
         />
         <motion.path
           strokeWidth="3"
-          className="stroke-secondary-foreground"
+          className="stroke-secondary"
           strokeLinecap="round"
           variants={{
             closed: { d: 'M 2 16.346 L 20 16.346' },
